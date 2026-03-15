b7970f87f5a516e8ffd126d0b7368d3cd8a013d9
inayat-ahmad <muqddaskhan230@gmail.com>
2026-03-15 19:19:20 +0530

    Home directory fix

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,21 @@
 import express from "express";
 import wrapAsync from "../utils/wrapAsync.js";
 import passport from "passport";
-import {redirectUrlFxn}  from "../middleware.js";
+import * as listingController from '../controllers/listings.js';
+import { redirectUrlFxn } from "../middleware.js";
 import * as userController from "../controllers/user.js"
 
 const router = express.Router();
 
-
+router.get("/", wrapAsync(listingController.index));
 router.route("/signup")
-.get(userController.renderSignupForm )
-.post(wrapAsync(userController.signup));
+    .get(userController.renderSignupForm)
+    .post(wrapAsync(userController.signup));
 
 router.route("/login")
-.get(userController.renderLoginForm)
-.post(redirectUrlFxn , passport.authenticate("local", { failureRedirect: "/login" , failureFlash: true }), userController.login);
+    .get(userController.renderLoginForm)
+    .post(redirectUrlFxn, passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), userController.login);
 
-router.get("/logout" , userController.logout);
+router.get("/logout", userController.logout);
 
 export default router;
